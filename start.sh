12209e17047b9f93bfc98828de8638776d24146b
PailloM <paillomams@gmx.com>
2025-11-24 23:37:26 -0500

    add MLFLOW_SERVER_X_FRAME_OPTIONS

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mlflow db upgrade mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE}
-mlflow server --backend-store-uri mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE} --default-artifact-root /artifacts --host 0.0.0.0 --port ${PORT} --allowed-hosts ${MLFLOW_SERVER_ALLOWED_HOSTS} --cors-allowed-origins ${MLFLOW_SERVER_CORS_ALLOWED_ORIGINS}
+mlflow server --backend-store-uri mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE} --default-artifact-root /artifacts --host 0.0.0.0 --port ${PORT} --allowed-hosts ${MLFLOW_SERVER_ALLOWED_HOSTS} --cors-allowed-origins ${MLFLOW_SERVER_CORS_ALLOWED_ORIGINS} --x-frame-options ${MLFLOW_SERVER_X_FRAME_OPTIONS}
 
