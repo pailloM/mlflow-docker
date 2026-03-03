d0cdf1dd912aec1ef44ec2404e3edcd43e89d693
PailloM <paillomams@gmx.com>
2026-03-02 23:54:51 -0500

    simplify if statement

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 mlflow db upgrade mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE}
 if ${MLFLOW_SERVER_DISABLE_SECURITY_MIDDLEWARE} = "true"
-  mlflow server --backend-store-uri mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE} --default-artifact-root /artifacts --host 0.0.0.0 --port ${PORT} --allowed-hosts ${MLFLOW_SERVER_ALLOWED_HOSTS} --cors-allowed-origins ${MLFLOW_SERVER_CORS_ALLOWED_ORIGINS} --x-frame-options ${MLFLOW_SERVER_X_FRAME_OPTIONS} --disable-security-middleware
+  mlflow server --backend-store-uri mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE} --default-artifact-root /artifacts --host 0.0.0.0 --port ${PORT} --disable-security-middleware
 else
   mlflow server --backend-store-uri mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE} --default-artifact-root /artifacts --host 0.0.0.0 --port ${PORT} --allowed-hosts ${MLFLOW_SERVER_ALLOWED_HOSTS} --cors-allowed-origins ${MLFLOW_SERVER_CORS_ALLOWED_ORIGINS} --x-frame-options ${MLFLOW_SERVER_X_FRAME_OPTIONS}
 fi
