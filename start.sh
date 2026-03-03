a1a78fa5039431bf7cb488f41c85fd4b9634eef2
PailloM <paillomams@gmx.com>
2026-03-02 23:58:51 -0500

    add missing then statement

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mlflow db upgrade mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE}
-if ${MLFLOW_SERVER_DISABLE_SECURITY_MIDDLEWARE} = "true"
+if ${MLFLOW_SERVER_DISABLE_SECURITY_MIDDLEWARE} = "true"; then
   mlflow server --backend-store-uri mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE} --default-artifact-root /artifacts --host 0.0.0.0 --port ${PORT} --disable-security-middleware
 else
   mlflow server --backend-store-uri mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb-mlflow/${MYSQL_DATABASE} --default-artifact-root /artifacts --host 0.0.0.0 --port ${PORT} --allowed-hosts ${MLFLOW_SERVER_ALLOWED_HOSTS} --cors-allowed-origins ${MLFLOW_SERVER_CORS_ALLOWED_ORIGINS} --x-frame-options ${MLFLOW_SERVER_X_FRAME_OPTIONS}
